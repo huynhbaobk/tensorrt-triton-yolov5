f7bbf2dc1f268c4eba2a3a8fca1ea4835b5ef634
huynhbaobk <huynhgiabaoa2@gmail.com>
2021-11-16 11:51:56 +0700

    Fix files sh

--- convert_tensorrt.sh
+++ convert_tensorrt.sh
@@ -1,10 +1,10 @@
-pip install -r requirements.txt
-
 wget https://github.com/ultralytics/yolov5/releases/download/v6.0/yolov5m6.pt -P yolov5
 
 cp tensorrtx/yolov5/gen_wts.py yolov5
 
 cd /workspace/yolov5
+
+pip install -r requirements.txt
 
 python gen_wts.py -w yolov5m6.pt -o yolov5m6.wts
 
@@ -12,7 +12,7 @@ cd /workspace/tensorrtx/yolov5
 
 rm -rf build
 
-mkdir build
+mkdir -p build
 
 cd build
 # update CLASS_NUM in yololayer.h if your model is trained on custom dataset
